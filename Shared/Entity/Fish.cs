859c5bd25f3ebbbeb6c7b4dd94a961bb8ac55553
Tatianapr8 <tatianapr.programacion@gmail.com>
2021-11-14 13:57:38 -0500

    prueba

---
+++ Shared/Entity/Fish.cs
@@ -0,0 +1,12 @@
+using System;
+namespace PracticaSept20.Shared.Entity
+{
+    public class Fish
+    {
+        public string FishSpecie {get;set;}
+        public string DescriptionSpecie {get;set;}
+        public string TypeOfWater {get;set;}
+        public string FishImage {get;set;}
+        public DateTime PublicationDate {get;set;}        
+    }
+}
